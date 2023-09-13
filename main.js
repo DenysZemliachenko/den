5d053da1fc4f7ecb57a4d15e0bb7d189ad72dccc
Denys Zemliachenko <den0zeracron@gmail.com>
2023-09-13 13:22:41 +0200

    Changed color and look in css and add article in html.Added js in test

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ console.log(name);
 console.log(age);
 console.log(`Siema drogi odwiedzający nazywam się ${name} i mam ${age} lat`);
 
+const heading = document.querySelector('.main-heading--js');
+console.log(heading);
