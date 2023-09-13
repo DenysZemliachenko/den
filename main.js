5a9fd294cb2f05b794145ec7eebb255a42d7d62b
Denys Zemliachenko <den0zeracron@gmail.com>
2023-09-13 13:47:00 +0200

    add new text with js and colored it

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ console.log(`Siema drogi odwiedzający nazywam się ${name} i mam ${age} lat`);
 
 const heading = document.querySelector('.main-heading--js');
 console.log(heading);
+
+const homework = document.querySelector('.homework--title');
+
+homework.innerHTML = 'Gdzie się uczę.';
+
+const homeworkdes = document.querySelector('.homework--description');
+
+homeworkdes.innerHTML = 'Uczę się w mieście Sopot w Technikum o nazwie Zespół Szkół Technicznych w Sopocie w 3 klasie';
