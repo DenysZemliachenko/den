642157c30de75c9cc744ed6328b35eaa5c88a75f
Denys Zemliachenko <den0zeracron@gmail.com>
2023-09-14 10:08:57 +0200

    upgrade my website

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-const name = 'Denys';
+const name = "Denys";
 const age = 16;
 
 console.log(name);
 console.log(age);
 console.log(`Siema drogi odwiedzający nazywam się ${name} i mam ${age} lat`);
 
-const heading = document.querySelector('.main-heading--js');
+const heading = document.querySelector(".main-heading--js");
+
 console.log(heading);
 
-const homework = document.querySelector('.homework--title');
+const homework = document.querySelector(".homework--title");
 
-homework.innerHTML = 'Gdzie się uczę.';
+homework.innerHTML = "Gdzie się uczę.";
 
-const homeworkdes = document.querySelector('.homework--description');
+const homeworkdes = document.querySelector(".homework--description");
 
-homeworkdes.innerHTML = 'Uczę się w mieście Sopot w Technikum o nazwie Zespół Szkół Technicznych w Sopocie w 3 klasie';
+homeworkdes.innerHTML =
+  "Uczę się w mieście Sopot w Technikum o nazwie Zespół Szkół Technicznych w Sopocie w 3 klasie";
